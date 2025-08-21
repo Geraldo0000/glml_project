c55f2311d8210e4e9fb4728a45e9d79041a9be03
Geraldo0000 <geratransporte@hotmail.com>
2025-08-21 09:35:57 -0300

    first project

---
+++ src/main/java/com/Geraldo/glml/GlmlApplication.java
@@ -0,0 +1,13 @@
+package com.Geraldo.glml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Geraldo/glml/GlmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Geraldo.glml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
