d06d9c8ecf4589c91e467fc8a9215beda231e2eb
Geraldo0000 <geratransporte@hotmail.com>
2025-08-22 09:55:00 -0300

    Game seed

--- src/main/java/com/Geraldo/glml/entities/Game.java
+++ src/main/java/com/Geraldo/glml/entities/Game.java
@@ -16,23 +16,49 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String imgUrl, String longDescription, String genre, String platform, String shortDescription, String title, Integer year) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
-        this.imgUrl = imgUrl;
-        this.longDescription = longDescription;
-        this.genre = genre;
-        this.platform = platform;
-        this.shortDescription = shortDescription;
         this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
         this.year = year;
     }
 
@@ -44,12 +70,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public Long getId() {
-        return id;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -60,22 +94,6 @@ public class Game {
         this.imgUrl = imgUrl;
     }
 
-    public String getLongDescription() {
-        return longDescription;
-    }
-
-    public void setLongDescription(String longDescription) {
-        this.longDescription = longDescription;
-    }
-
-    public String getPlatform() {
-        return platform;
-    }
-
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-
     public String getShortDescription() {
         return shortDescription;
     }
@@ -83,21 +101,12 @@ public class Game {
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
-
-    public String getTitle() {
-        return title;
+    public String getLongDescription() {
+        return longDescription;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
     }
 
     @Override
